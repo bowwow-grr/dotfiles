8a81e541268e31bd204a264cb20b7415c9df09f8
bowwow-grr <std8zipped@gmail.com>
2019-06-10 23:14:13 +0900

    add settings

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+rm -r ~/.config/nvim
+rm ~/.tmux.conf
+rm ~/.bashrc
+rm ~/.bash_logout
+
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.bashrc ~/.bashrc
