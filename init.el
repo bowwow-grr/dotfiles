5ee6b66d9fbebd127c89efd91a657eb3a614d145
bowwow-grr <std8zipped@gmail.com>
2019-08-05 02:45:25 +0900

    install.shを少し変更

--- init.el
+++ init.el
@@ -12,10 +12,22 @@
 (package-initialize)
 
 ;;; mozc
-(require 'mozc)					;; なぜかこのあたりでエラーが出る
-(set-language-environment "Japanese") 	;; 言語環境を"Japaneseに
+;(require 'mozc)					;; なぜかこのあたりでエラーが出る
+;(set-language-environment "Japanese") 	;; 言語環境を"Japaneseに
 (setq default-input-method "japanese-mozc") 	;; IMEをjapanese-mozc
 (prefer-coding-system 'utf-8) 			;;; デフォルトの文字コードをUTF-8
 
 ;;; 入力切り替え
 (global-set-key (kbd "C-j") 'toggle-input-method)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 DOTFILES=$HOME/dotfiles
+DOTCOFIG=$HOME/.config
 NVIM_DIR=$HOME/.config/nvim
 TMUX_CONF=$HOME/.tmux.conf
 BASHRC=$HOME/.bashrc
 BASH_LOGOUT=$HOME/.bash_logout
 INIT_EL=$HOME/.emacs.d/init.el 
+
+if [ ! -e ${DOTCOFIG} ]; then eval 'mkdir ${DOTCOFIG}'
 
 ln -s $DOTFILES/nvim $NVIM_DIR 
 ln -s $DOTFILES/.tmux.conf $TMUX_CONF
