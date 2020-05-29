14d40b5f983a5aecaad833eb5fe6fa7545f6b3ad
bowwow-grr <std8zipped@gmail.com>
2020-05-30 01:49:26 +0900

    emacsの設定を見直した

--- init.el
+++ init.el
@@ -1,34 +1,7 @@
-;;; package.el
-(require 'package)
-;; MELPAを追加
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;; MELPA-stableを追加
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-;; Marmaladeを追加
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-;; Orgを追加
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
-;; 初期化
 (package-initialize)
+(setq package-archives
+      '(("gnu". "http://elpa.gnu.org/packages/")
+        ("melpa". "http://melpa.org/packages/")
+        ("org". "http://orgmode.org/elpa/")))
 
-;;; mozc
-;(require 'mozc)					;; なぜかこのあたりでエラーが出る
-;(set-language-environment "Japanese") 	;; 言語環境を"Japaneseに
-(setq default-input-method "japanese-mozc") 	;; IMEをjapanese-mozc
-(prefer-coding-system 'utf-8) 			;;; デフォルトの文字コードをUTF-8
-
-;;; 入力切り替え
-(global-set-key (kbd "C-j") 'toggle-input-method)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark)))
- '(package-selected-packages (quote (evil))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(load-theme 'monokai t)
